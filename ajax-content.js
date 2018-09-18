46302a4d9020345585e794b384523133e4fbfaeb
Michele Capicchioni <capimichi@gmail.com>
2018-09-18 22:12:02 +0200

    Fix loader color

--- ajax-content.js
+++ ajax-content.js
@@ -1,7 +1,7 @@
 jQuery(function () {
     var $ajaxContent = {};
     $ajaxContent.loaders = {
-        oval: '<!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL --> <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"> <g fill="none" fill-rule="evenodd"> <g transform="translate(1 1)" stroke-width="2"> <circle stroke-opacity=".5" cx="18" cy="18" r="18"/> <path d="M36 18c0-9.94-8.06-18-18-18"> <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/> </path> </g> </g> </svg>'
+        oval: '<!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL --> <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="{color}"> <g fill="none" fill-rule="evenodd"> <g transform="translate(1 1)" stroke-width="2"> <circle stroke-opacity=".5" cx="18" cy="18" r="18"/> <path d="M36 18c0-9.94-8.06-18-18-18"> <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/> </path> </g> </g> </svg>'
     };
     $ajaxContent.utils = {};
     $ajaxContent.functions = {};
@@ -19,6 +19,8 @@ jQuery(function () {
         var $t = jQuery(this);
 
         var $loaderImage = $ajaxContent.loaders.oval;
+        var $loaderColor = $ajaxContent.utils.hasAttr($t, 'data-loader-color') ? $t.attr('data-loader-color') : '#fff';
+        $loaderImage = $loaderImage.replace(/{color}/g, $loaderColor);
         var $loaderHeight = $ajaxContent.utils.hasAttr($t, 'data-loader-height') ? $t.attr('data-loader-height') : '200px';
         var $loaderWidth = $ajaxContent.utils.hasAttr($t, 'data-loader-width') ? $t.attr('data-loader-width') : '100%';
         var $maxRetry = $ajaxContent.utils.hasAttr($t, 'data-max-retry') ? parseInt($t.attr('data-max-retry')) : 5;
